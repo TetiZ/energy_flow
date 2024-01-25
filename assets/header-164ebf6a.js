e29e5f2d1380649c56403e564bade709f89407a4
TetiZ <140248456+TetiZ@users.noreply.github.com>
2024-01-25 18:49:23 +0000

    Deploying to gh-pages from @ TetiZ/energy_flow@fab37202c293f3f123f5874d9e7ac0f3903a3882 🚀

---
+++ assets/header-164ebf6a.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("#Home"),a=document.querySelector("#Favorites");e.addEventListener("click",()=>{e.classList.add("heder-nav-link-active"),a.classList.remove("heder-nav-link-active")});a.addEventListener("click",()=>{a.classList.add("heder-nav-link-active"),e.classList.remove("heder-nav-link-active")});
+//# sourceMappingURL=header-164ebf6a.js.map
