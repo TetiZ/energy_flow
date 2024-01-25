e5358043d1cc65122adf9e19beca441badddd72e
TetiZ <140248456+TetiZ@users.noreply.github.com>
2024-01-25 19:40:57 +0000

    Deploying to gh-pages from @ TetiZ/energy_flow@77f54288867ba4c77e7e11af90f52615abe23222 🚀

---
+++ assets/pop-ups-dfa62cc2.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".pop-close-btn"),o=document.querySelector(".backdrop-ex"),n=document.querySelector(".pop-rating-btn"),t=document.querySelector(".backdrop");e.addEventListener("click",function(){o.classList.remove("is-open")});n.addEventListener("click",function(){t.classList.toggle("is-open")});
+//# sourceMappingURL=pop-ups-dfa62cc2.js.map
