01100eb1c3faf2632e14a5a8b8337ed56c6c30ff
Nazar-Kotiv <kotnazar20@gmail.com>
2024-01-25 20:03:13 +0100

    commit 6

---
+++ src/main.js
@@ -0,0 +1,9 @@
+<<<<<<< Updated upstream
+=======
+import('./js/api');
+import('./js/mobile-menu');
+// import('./js/exersises');
+import('./js/formFooter');
+import('./js/quote');
+import('./js/header');
+>>>>>>> Stashed changes
