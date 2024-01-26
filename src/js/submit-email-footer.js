9e76c5fb2c817d495e578fed72b870036ad7f7fd
TetiZ <tommytaba99@gmail.com>
2024-01-26 16:17:52 +0100

    submit-form-update

--- src/js/submit-email-footer.js
+++ src/js/submit-email-footer.js
@@ -1,21 +1,47 @@
 const form = document.querySelector('.subs-form');
 const localStorageKey = 'user-email';
-let userEmail;
-let allUsersEmails = [];
 
 form.elements.email.value = localStorage.getItem(localStorageKey) ?? '';
 
-form.addEventListener('input', saveToLocalStogae);
+form.addEventListener('input', saveToLocalStorage);
 
-function saveToLocalStogae(evt) {
+function saveToLocalStorage(evt) {
   localStorage.setItem(localStorageKey, evt.target.value);
-  userEmail = localStorage.getItem(localStorageKey);
 }
 
-form.addEventListener('submit', evt => {
-  evt.preventDefault();
-  allUsersEmails.push(userEmail);
-  console.log(allUsersEmails);
-  localStorage.removeItem(localStorageKey);
-  form.reset();
+function postEmail(userEmail) {
+  fetch('https://energyflow.b.goit.study/api/subscription', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ emails: [userEmail] }),
+  })
+    .then(response => {
+      if (!response.ok) {
+        console.error('Код помилки:', response.status);
+        throw new Error('Мережевий запит не був успішним');
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log('Дані успішно відправлені', data);
+    })
+    .catch(error => {
+      console.error('Помилка при відправленні даних', error);
+    });
+}
+
+form.addEventListener('submit', async e => {
+  e.preventDefault();
+
+  const userEmail = localStorage.getItem(localStorageKey);
+
+  try {
+    await postEmail(userEmail);
+    localStorage.removeItem(localStorageKey);
+    form.reset();
+  } catch (error) {
+    console.log('Не знайдено імейл для відправлення.');
+  }
 });
