563a0f8c0b7b5ee1f61851f27c98111757ff868b
TetiZ <140248456+TetiZ@users.noreply.github.com>
2024-01-26 14:49:13 +0000

    Deploying to gh-pages from @ TetiZ/energy_flow@9020ee5b888ad6e1e61b5f9ef7b6081125414cd6 🚀

--- assets/exersises-d4bba101.js
+++ assets/exersises-d4bba101.js
@@ -1,4 +1,4 @@
-import{musclesGroup as $,bodyPart as w}from"./api-f9182e38.js";const S=document.querySelector(".exercise-cards-list"),y=document.querySelector(".exercise-cards-list");document.querySelectorAll(".exercise-card");const p=document.querySelector(".exercise-pages-counter");document.querySelector(".exercise-parts");const P=document.querySelector(".exercise-form"),v=document.querySelector(".exercise-input-button"),b=document.querySelector(".exercise-input"),E=document.querySelectorAll(".exercise-button"),a=document.querySelector(".exercise-parts-list");let o,f="",x=[],h;localStorage.removeItem("searchInput");localStorage.removeItem("searchQuery");S.addEventListener("click",async i=>{i.preventDefault(),x=[],o=1;try{const e=(await $(i)).results;if(x=[...e],e.length===0)throw new Error({title:"No Results",message:"No images found. Please try a different search term."});y.style.display="none",a.style.display="flex",a.innerHTML=e.reduce((s,r)=>s+`
+import{musclesGroup as w,bodyPart as $}from"./api-f9182e38.js";const S=document.querySelector(".exercise-cards-list"),b=document.querySelector(".exercise-cards-list");document.querySelectorAll(".exercise-card");const g=document.querySelector(".exercise-pages-counter");document.querySelector(".exercise-parts");const P=document.querySelector(".exercise-form"),v=document.querySelector(".exercise-input-button"),m=document.querySelector(".exercise-input"),E=document.querySelectorAll(".exercise-button"),a=document.querySelector(".exercise-parts-list");let o,f="",x=[],h;localStorage.removeItem("searchInput");localStorage.removeItem("searchQuery");S.addEventListener("click",async i=>{i.preventDefault(),x=[],o=1;try{const e=(await w(i)).results;if(x=[...e],e.length===0)throw new Error({title:"No Results",message:"No images found. Please try a different search term."});b.style.display="none",a.style.display="flex",a.innerHTML=e.reduce((s,r)=>s+`
         <li class="exercise-parts">
             <div class="part-container">
                 <div class="exercise-head-container">
@@ -9,7 +9,13 @@ import{musclesGroup as $,bodyPart as w}from"./api-f9182e38.js";const S=document.
                         <use href="../img/icons.svg#icon-star"></use>
                     </svg>
                     </span>
-                <button class="exercise-part-button">Start</button>
+                <a class="exercise-part-link" href="../partials/pop-up-exersise-card.html">
+                <button class="exercise-part-button">Start   
+                <svg class="exercise-btn-icon" width="14" height="14">
+                <use href="../img/icons.svg#icon-arrow-right"></use>
+                </svg>
+                </button>
+                </a>
                 </div>
 
                 <!-- <svg class="exercise-btn-icon">
@@ -37,7 +43,7 @@ import{musclesGroup as $,bodyPart as w}from"./api-f9182e38.js";const S=document.
                 </ul>
             </div>
             </li>
-        `,"")}catch(c){console.error("Error:",c)}});p.addEventListener("click",async i=>{o=i.target.textContent,console.log(o),h=8;try{const s=JSON.parse(localStorage.getItem("searchQuery")).searchQuery;console.log(s);const r=`https://energyflow.b.goit.study/api/exercises?muscles=${s}&page=${o}&limit=${h}`,d=await fetch(r);if(d.status!==200)throw new Error("Failed to fetch data. Please try again later.");const u=(await d.json()).results;console.log(u);const m=Math.round(u.rating);a.innerHTML=u.reduce((n,l)=>n+`
+        `,"")}catch(c){console.error("Error:",c)}});g.addEventListener("click",async i=>{o=i.target.textContent,console.log(o),h=8;try{const s=JSON.parse(localStorage.getItem("searchQuery")).searchQuery;console.log(s);const r=`https://energyflow.b.goit.study/api/exercises?muscles=${s}&page=${o}&limit=${h}`,p=await fetch(r);if(p.status!==200)throw new Error("Failed to fetch data. Please try again later.");const u=(await p.json()).results;console.log(u);const y=Math.round(u.rating);a.innerHTML=u.reduce((n,l)=>n+`
         <li class="exercise-parts">
             <div class="part-container">
                 <div class="exercise-head-container">
@@ -48,7 +54,13 @@ import{musclesGroup as $,bodyPart as w}from"./api-f9182e38.js";const S=document.
                         <use href="../img/icons.svg#icon-star"></use>
                     </svg>
                     </span>
-                <button class="exercise-part-button">Start</button>
+               <a class="exercise-part-link" href="../partials/pop-up-exersise-card.html">
+                <button class="exercise-part-button">Start   
+                <svg class="exercise-btn-icon" width="14" height="14">
+                <use href="../img/icons.svg#icon-arrow-right"></use>
+                </svg>
+                </button>
+                </a>
                 </div>
 
                 <!-- <svg class="exercise-btn-icon">
@@ -76,7 +88,7 @@ import{musclesGroup as $,bodyPart as w}from"./api-f9182e38.js";const S=document.
                 </ul>
             </div>
             </li>
-        `,"")}catch(e){console.error("Error:",e)}});E.forEach(i=>{i.addEventListener("click",c=>{c.preventDefault();const e=c.target.textContent;localStorage.removeItem("searchInput"),localStorage.removeItem("searchQuery"),e==="Muscles"?(b.style.display="none",v.style.display="none",a.style.display="none",y.style.display="flex"):e==="Body parts"&&(a.innerHTML="",b.style.display="flex",v.style.display="flex",a.style.display="flex",y.style.display="none",p.style.display="none")})});P.addEventListener("submit",async function(i){i.preventDefault(),x=[],o=1;try{const c=b.value,s=(await w(c)).results;if(x=[...s],console.log(s),s.length===0){console.log(s.length),a.insertAdjacentHTML("beforeend","<li class='no-result-list'><p class='no-result'>Unfortunately,<span class='no-result-span'> no results</span> were found.You may want to consider other search options to find the exercise you are looking for. Our range is wide and you have the opportunity to find more options that suit your needs.</p>"),p.style.display="none";return}else{p.style.display="flex";const r=Math.round(s.rating);a.innerHTML=s.reduce((d,t)=>d+`
+        `,"")}catch(e){console.error("Error:",e)}});E.forEach(i=>{i.addEventListener("click",c=>{c.preventDefault();const e=c.target.textContent;localStorage.removeItem("searchInput"),localStorage.removeItem("searchQuery"),e==="Muscles"?(m.style.display="none",v.style.display="none",a.style.display="none",b.style.display="flex"):e==="Body parts"&&(a.innerHTML="",m.style.display="flex",v.style.display="flex",a.style.display="flex",b.style.display="none",g.style.display="none")})});P.addEventListener("submit",async function(i){i.preventDefault(),x=[],o=1;try{const c=m.value,s=(await $(c)).results;if(x=[...s],console.log(s),s.length===0){console.log(s.length),a.insertAdjacentHTML("beforeend","<li class='no-result-list'><p class='no-result'>Unfortunately,<span class='no-result-span'> no results</span> were found.You may want to consider other search options to find the exercise you are looking for. Our range is wide and you have the opportunity to find more options that suit your needs.</p>"),g.style.display="none";return}else{g.style.display="flex";const r=Math.round(s.rating);a.innerHTML=s.reduce((p,t)=>p+`
         <li class="exercise-parts">
             <div class="part-container">
                 <div class="exercise-head-container">
@@ -87,7 +99,13 @@ import{musclesGroup as $,bodyPart as w}from"./api-f9182e38.js";const S=document.
                         <use href="../img/icons.svg#icon-star"></use>
                     </svg>
                     </span>
-                <button class="exercise-part-button">Start</button>
+                <a class="exercise-part-link" href="../partials/pop-up-exersise-card.html">
+                <button class="exercise-part-button">Start   
+                <svg class="exercise-btn-icon" width="14" height="14">
+                <use href="../img/icons.svg#icon-arrow-right"></use>
+                </svg>
+                </button>
+                </a>
                 </div>
 
                 <!-- <svg class="exercise-btn-icon">
@@ -115,7 +133,7 @@ import{musclesGroup as $,bodyPart as w}from"./api-f9182e38.js";const S=document.
                 </ul>
             </div>
             </li>
-        `,"")}}catch(c){console.error("Error:",c)}});p.addEventListener("click",async i=>{o=i.target.textContent,h=8;try{f=JSON.parse(localStorage.getItem("searchInput")).searchInput;const s=`https://energyflow.b.goit.study/api/exercises?bodypart=${f}&page=${o}&limit=${h}`,r=await fetch(s);if(r.status!==200)throw new Error("Failed to fetch data. Please try again later.");const g=(await r.json()).results;console.log(g.length);const u=Math.round(g.rating);a.innerHTML=g.reduce((m,n)=>m+`
+        `,"")}}catch(c){console.error("Error:",c)}});g.addEventListener("click",async i=>{o=i.target.textContent,h=8;try{f=JSON.parse(localStorage.getItem("searchInput")).searchInput;const s=`https://energyflow.b.goit.study/api/exercises?bodypart=${f}&page=${o}&limit=${h}`,r=await fetch(s);if(r.status!==200)throw new Error("Failed to fetch data. Please try again later.");const d=(await r.json()).results;console.log(d.length);const u=Math.round(d.rating);a.innerHTML=d.reduce((y,n)=>y+`
         <li class="exercise-parts">
             <div class="part-container">
                 <div class="exercise-head-container">
@@ -126,7 +144,13 @@ import{musclesGroup as $,bodyPart as w}from"./api-f9182e38.js";const S=document.
                         <use href="../img/icons.svg#icon-star"></use>
                     </svg>
                     </span>
-                <button class="exercise-part-button">Start</button>
+              <a class="exercise-part-link" href="../partials/pop-up-exersise-card.html">
+                <button class="exercise-part-button">Start   
+                <svg class="exercise-btn-icon" width="14" height="14">
+                <use href="../img/icons.svg#icon-arrow-right"></use>
+                </svg>
+                </button>
+                </a>
                 </div>
 
                 <!-- <svg class="exercise-btn-icon">
@@ -155,4 +179,4 @@ import{musclesGroup as $,bodyPart as w}from"./api-f9182e38.js";const S=document.
             </div>
             </li>
         `,"")}catch(e){console.error("Error:",e)}});
-//# sourceMappingURL=exersises-28c57028.js.map
+//# sourceMappingURL=exersises-d4bba101.js.map
