fcec4e6ecb3bdbf1c87c116ed43aaa5c134fb5bb
TetiZ <140248456+TetiZ@users.noreply.github.com>
2024-01-26 15:18:15 +0000

    Deploying to gh-pages from @ TetiZ/energy_flow@9e76c5fb2c817d495e578fed72b870036ad7f7fd 🚀

--- assets/submit-email-footer-7378164b.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".subs-form"),t="user-email";let o,a=[];e.elements.email.value=localStorage.getItem(t)??"";e.addEventListener("input",r);function r(l){localStorage.setItem(t,l.target.value),o=localStorage.getItem(t)}e.addEventListener("submit",l=>{l.preventDefault(),a.push(o),console.log(a),localStorage.removeItem(t),e.reset()});
-//# sourceMappingURL=submit-email-footer-7378164b.js.map
