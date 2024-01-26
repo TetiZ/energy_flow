42a7ee0c6f1cf6e5602a85c049e2f7573b43cd1e
Anastasia Romanyuk <vnstyv@gmail.com>
2024-01-26 19:47:03 +0200

    ed

--- src/js/izitoast.js
+++ src/js/izitoast.js
@@ -9,7 +9,7 @@ form.addEventListener('submit', function (e) {
 
   iziToast.info({
     title: 'Information',
-    message: 'Form submitted',
+    message: 'We are excited to have you on board! Thank you for subscribing to new exercises on Energy Flow. You have just taken a significant step towards improving your fitness and well-being.',
     position: 'topRight',
     icon: 'fas fa-info-circle', 
     close: false, 
@@ -26,19 +26,42 @@ const ratingBtn = popBtnsContainer.querySelector('.pop-rating-btn');
 addFavBtn.addEventListener('click', function () {
   iziToast.success({
     title: 'Success',
-    message: 'Added to favorites',
+    message: 'Excellent choice! The exercise has been successfully added to your favorites. Get ready for endless inspiration and great results!',
     position: 'topRight',
     icon: 'fas fa-thumbs-up', 
   });
 });
 
 ratingBtn.addEventListener('click', function () {
-  iziToast.info({
-    title: 'Information',
-    message: 'Your rating has been saved',
-    position: 'topRight',
-    icon: 'fas fa-star', 
+
+  fetch('your-api-endpoint', {
+    method: 'POST',
+    body: new FormData(form),
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Failed to send request. Please try again.'); 
+    }
+    return response.json();
+  })
+  .then(data => {
+    iziToast.info({
+      title: 'Information',
+      message: 'Thank you for your comment and rating! Your words are motivation for us. Together we will reach new heights.',
+      position: 'topRight',
+      icon: 'fas fa-star', 
+    });
+  })
+  .catch(error => {
+    iziToast.error({
+      title: 'Error',
+      message: error.message,
+      position: 'topRight',
+      icon: 'fas fa-times', 
+    });
   });
 });
+
+
 
 
