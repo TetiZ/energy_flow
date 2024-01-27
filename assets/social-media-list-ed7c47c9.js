b15c1b452d7c03ad9f63f8ebd37f0b8df1f49750
TetiZ <140248456+TetiZ@users.noreply.github.com>
2024-01-27 18:31:13 +0000

    Deploying to gh-pages from @ TetiZ/energy_flow@1b3f2b89847638051006d2aea86bcaa9615d41c0 🚀

---
+++ assets/social-media-list-ed7c47c9.js
@@ -0,0 +1,34 @@
+document.querySelector(".sm-list");document.querySelector(".heder-link-list");const a="heder-link-list",s="heder-link-item",e="heder-link",l="heder-link-icon",i=`<li class="${s}">
+            <a
+              class="${e}"
+              href="https://www.facebook.com/goITclub/"
+              target="_blank"
+            >
+              <svg class="${l}" aria-label="facebook icon">
+                <use href="./img/icons.svg#icon-facebook"></use>
+              </svg>
+            </a>
+          </li>
+          <li class="${s}">
+            <a
+              class="${e}"
+              href="https://www.instagram.com/goitclub/"
+              target="_blank"
+            >
+              <svg class="${l}" aria-label="instagram icon">
+                <use href="./img/icons.svg#icon-instagram"></use>
+              </svg>
+            </a>
+          </li>
+          <li class="${s}">
+            <a
+              class="${e}"
+              href="https://www.youtube.com/c/GoIT"
+              target="_blank"
+            >
+              <svg class="${l}" aria-label="youtube icon">
+                <use href="./img/icons.svg#icon-youtube"></use>
+              </svg>
+            </a>
+          </li>`;document.querySelector(`.${a}`).innerHTML=i;
+//# sourceMappingURL=social-media-list-ed7c47c9.js.map
