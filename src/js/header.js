10f6a1609b5bf3eec984513396571069b6cbfce2
TetiZ <tommytaba99@gmail.com>
2024-01-28 13:19:00 +0100

    update header.js

--- src/js/header.js
+++ src/js/header.js
@@ -1,17 +1,19 @@
 const homePage = document.querySelector('#Home');
 const favoritesPage = document.querySelector('#Favorites');
 
-document.addEventListener('DOMContentLoaded', function () {
+window.addEventListener('load', setHomePageActive);
+
+function setHomePageActive() {
   homePage.classList.add('heder-nav-link-active');
-});
+}
 
 function setActivePage(pageId) {
-  homePage.classList.remove('heder-nav-link-active');
-  favoritesPage.classList.remove('heder-nav-link-active');
+  window.removeEventListener('load', setHomePageActive);
 
   if (pageId === 'Home') {
     homePage.classList.add('heder-nav-link-active');
   } else if (pageId === 'Favorites') {
+    homePage.classList.remove('heder-nav-link-active');
     favoritesPage.classList.add('heder-nav-link-active');
   }
 }
