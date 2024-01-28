aaf4a1545ca73fd8429afda325d37f7d42d638cb
TetiZ <tommytaba99@gmail.com>
2024-01-29 00:14:29 +0100

    MAIN exercises.js

--- src/js/exersises.js
+++ src/js/exersises.js
@@ -102,18 +102,18 @@ exerciseList.addEventListener('click', async event => {
                 <a class="exercise-part-link" href="../partials/pop-up-exersise-card.html">
                 <button class="exercise-part-button"  id='${currentId}'>Start
                 <svg class="exercise-btn-icon" width="14" height="14">
-                <use href=".img/icons.svg#icon-arrow-right"></use>
+                <use href="./img/icons.svg#icon-arrow-right"></use>
                 </svg>
                 </button>
                 </a>
                 </div>
 
                 <!-- <svg class="exercise-btn-icon">
-                <use href=".img/icons.svg#icon-arrow-right"></use>
+                <use href="./img/icons.svg#icon-arrow-right"></use>
                 </svg> -->
                 <span class="exercise-part-name">
                 <svg class="exercise-part-icon" width="24" height="24">
-                    <use href=".img/icons.svg#icon-running-man"></use>
+                    <use href="./img/icons.svg#icon-running-man"></use>
                 </svg>
                 <p class="exercise-name-text">${
                   result.name.substring(0, 24) + '...'
@@ -193,7 +193,7 @@ pageCounter.addEventListener('click', async event => {
                     <a class="exercise-part-link" href="../partials/pop-up-exersise-card.html">
                 <button class="exercise-part-button"  id='${paginationId}'>Start
                 <svg class="exercise-btn-icon" width="14" height="14">
-                <use href=".img/icons.svg#icon-arrow-right"></use>
+                <use href="./img/icons.svg#icon-arrow-right"></use>
                 </svg>
                 </button>
                 </a>
@@ -204,7 +204,7 @@ pageCounter.addEventListener('click', async event => {
                 </svg> -->
                 <span class="exercise-part-name">
                 <svg class="exercise-part-icon" width="24" height="24">
-                    <use href=".img/icons.svg#icon-running-man"></use>
+                    <use href="./img/icons.svg#icon-running-man"></use>
                 </svg>
                 <p class="exercise-name-text">${
                   result.name.substring(0, 24) + '...'
@@ -604,3 +604,67 @@ exercisePartsList.addEventListener('click', async event => {
     }
   });
 });
+
+// FAVORITES
+const savedCards = JSON.parse(localStorage.getItem('exercises'));
+console.log(savedCards);
+
+const favList = document.querySelector('.fav-list');
+const favoritesPage = document.querySelector('#Favorites');
+
+favoritesPage.addEventListener('click', renderCardsFromStorage);
+
+function renderCardsFromStorage(e) {
+  const savedCards = JSON.parse(localStorage.getItem('exercises'));
+  console.log(savedCards);
+
+  favList.innerHTML = savedCards
+    .map(
+      ({ bodyPart, name, target, burnedCalories }) => `
+    <li class="exercise-parts">
+      <div class="part-container">
+        <div class="exercise-head-container">
+          <span class="exercise-badge">WORKOUT</span>
+          <button class="exercise-trash-button">
+            <svg class="exercise-trash-icon" width="16" height="16">
+              <use href="./img/icons.svg#icon-trash"></use>
+            </svg>
+          </button>
+          <a
+            class="exercise-part-link"
+            href="./partials/pop-up-exercise-card.html"
+          >
+            <button class="exercise-part-button">
+              Start
+              <svg class="exercise-btn-icon" width="14" height="14">
+                <use href="./img/icons.svg#icon-arrow-right"></use>
+              </svg>
+            </button>
+          </a>
+        </div>
+        <span class="exercise-part-name">
+          <svg class="exercise-part-icon" width="24" height="24">
+            <use href="./img/icons.svg#icon-running-man"></use>
+          </svg>
+          <p class="exercise-name-text">${name}</p> 
+        </span>
+        <ul class="exercise-describes-list">
+          <li class="exercise-describe">
+            <p class="exercise-describe-category">Burned calories:</p>
+            <p class="exercise-describing">${burnedCalories} / 3 min</p>
+          </li>
+          <li class="exercise-describe">
+            <p class="exercise-describe-category">Body part:</p>
+            <p class="exercise-describing">${bodyPart}</p>
+          </li>
+          <li class="exercise-describe">
+            <p class="exercise-describe-category">Target:</p>
+            <p class="exercise-describing">${target}</p>
+          </li>
+        </ul>
+      </div>
+    </li>
+  `
+    )
+    .join('');
+}
