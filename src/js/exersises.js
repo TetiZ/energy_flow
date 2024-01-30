9ba0faa3c9f841e1efaad7162793e5ce05111d69
TetiZ <tommytaba99@gmail.com>
2024-01-30 22:17:19 +0100

    svg path update

--- src/js/exersises.js
+++ src/js/exersises.js
@@ -97,25 +97,25 @@ exerciseList.addEventListener('click', async event => {
                       result.rating
                     )}</p>
                     <svg class="exercise-rating-icon" width="18" height="18">
-                        <use href="/energy_flow/assets/icons-de67b048.svg#icon-star"></use>
+                        <use href="/energy_flow/assets/icons.svg#icon-star"></use>
                     </svg>
                     </span>
 
                 <a class="exercise-part-link" href="../partials/pop-up-exersise-card.html">
                 <button class="exercise-part-button"  id='${currentId}'>Start
                 <svg class="exercise-btn-icon" width="14" height="14">
-                <use href="/energy_flow/assets/icons-de67b048.svg#icon-arrow-right"></use>
+                <use href="/energy_flow/assets/icons.svg#icon-arrow-right"></use>
                 </svg>
                 </button>
                 </a>
                 </div>
 
                 <!-- <svg class="exercise-btn-icon">
-                <use href="/energy_flow/assets/icons-de67b048.svg#icon-arrow-right"></use>
+                <use href="/energy_flow/assets/icons.svg#icon-arrow-right"></use>
                 </svg> -->
                 <span class="exercise-part-name">
                 <svg class="exercise-part-icon" width="24" height="24">
-                    <use href="/energy_flow/assets/icons-de67b048.svg#icon-running-man"></use>
+                    <use href="/energy_flow/assets/icons.svg#icon-running-man"></use>
                 </svg>
                 <p class="exercise-name-text">${
                   result.name.substring(0, 24) + '...'
@@ -189,13 +189,13 @@ pageCounter.addEventListener('click', async event => {
                       result.rating
                     )}</p>
                     <svg class="exercise-rating-icon" width="18" height="18">
-                        <use href="./energy_flow/assets/icons-de67b048.svg#icon-star"></use>
+                        <use href="./energy_flow/assets/icons.svg#icon-star"></use>
                     </svg>
                     </span>
                     <a class="exercise-part-link" href="../partials/pop-up-exersise-card.html">
                 <button class="exercise-part-button"  id='${paginationId}'>Start
                 <svg class="exercise-btn-icon" width="14" height="14">
-                <use href="./energy_flow/assets/icons-de67b048.svg#icon-arrow-right"></use>
+                <use href="./energy_flow/assets/icons.svg#icon-arrow-right"></use>
                 </svg>
                 </button>
                 </a>
@@ -203,7 +203,7 @@ pageCounter.addEventListener('click', async event => {
 
                 <span class="exercise-part-name">
                 <svg class="exercise-part-icon" width="24" height="24">
-                    <use href="/energy_flow/assets/icons-de67b048.svg#icon-running-man"></use>
+                    <use href="/energy_flow/assets/icons.svg#icon-running-man"></use>
                 </svg>
                 <p class="exercise-name-text">${
                   result.name.substring(0, 24) + '...'
@@ -317,24 +317,24 @@ exerciseForm.addEventListener('submit', async function (event) {
                       result.rating
                     )}</p>
                     <svg class="exercise-rating-icon" width="18" height="18">
-                        <use href="./energy_flow/assets/icons-de67b048.svg#icon-star"></use>
+                        <use href="./energy_flow/assets/icons.svg#icon-star"></use>
                     </svg>
                     </span>
                 <a class="exercise-part-link" href="../partials/pop-up-exersise-card.html">
                 <button class="exercise-part-button"  id='${inputId}'>Start
                 <svg class="exercise-btn-icon" width="14" height="14">
-                <use href="/energy_flow/assets/icons-de67b048.svg#icon-arrow-right"></use>
+                <use href="/energy_flow/assets/icons.svg#icon-arrow-right"></use>
                 </svg>
                 </button>
                 </a>
                 </div>
 
                 <!-- <svg class="exercise-btn-icon">
-                <use href="/energy_flow/assets/icons-de67b048.svg#icon-arrow-right"></use>
+                <use href="/energy_flow/assets/icons.svg#icon-arrow-right"></use>
                 </svg> -->
                 <span class="exercise-part-name">
                 <svg class="exercise-part-icon" width="24" height="24">
-                    <use href="/energy_flow/assets/icons-de67b048.svg#icon-running-man"></use>
+                    <use href="/energy_flow/assets/icons.svg#icon-running-man"></use>
                 </svg>
                 <p class="exercise-name-text">${
                   result.name.substring(0, 24) + '...'
@@ -410,13 +410,13 @@ pageCounter.addEventListener('click', async event => {
                       result.rating
                     )}</p>
                     <svg class="exercise-rating-icon" width="18" height="18">
-                        <use href="/energy_flow/assets/icons-de67b048.svg#icon-star"></use>
+                        <use href="/energy_flow/assets/icons.svg#icon-star"></use>
                     </svg>
                     </span>
                     <a class="exercise-part-link" href="./partials/pop-up-exersise-card.html">
                 <button class="exercise-part-button"  id='${inputPagination}'>Start
                 <svg class="exercise-btn-icon" width="14" height="14">
-                <use href="/energy_flow/assets/icons-de67b048.svg#icon-arrow-right"></use>
+                <use href="/energy_flow/assets/icons.svg#icon-arrow-right"></use>
                 </svg>
                 </button>
                 </a>
@@ -424,7 +424,7 @@ pageCounter.addEventListener('click', async event => {
 
                 <span class="exercise-part-name">
                 <svg class="exercise-part-icon" width="24" height="24">
-                    <use href="/energy_flow/assets/icons-de67b048.svg#icon-running-man"></use>
+                    <use href="/energy_flow/assets/icons.svg#icon-running-man"></use>
                 </svg>
                 <p class="exercise-name-text">${
                   result.name.substring(0, 24) + '...'
@@ -468,7 +468,7 @@ function popUp(data) {
           height="24"
           aria-label="close icon"
         >
-          <use href="/energy_flow/assets/icons-de67b048.svg#icon-close"></use>
+          <use href="/energy_flow/assets/icons.svg#icon-close"></use>
         </svg>
       </button>
       <div class="pop-exercises-img">
@@ -492,7 +492,7 @@ function popUp(data) {
                 width="18"
                 height="18"
                 aria-label="ratting">
-                <use href="/energy_flow/assets/icons-de67b048.svg#icon-star"></use>
+                <use href="/energy_flow/assets/icons.svg#icon-star"></use>
               </svg>
             </li>
         </ul>
@@ -531,7 +531,7 @@ function popUp(data) {
         <div class="pop-btns-container">
           <button class="pop-add-fav">
             Add to favorites<svg class="heart-icon" width="18" height="18">
-              <use href="/energy_flow/assets/icons-de67b048.svg#icon-heart"></use>
+              <use href="/energy_flow/assets/icons.svg#icon-heart"></use>
             </svg>
           </button>
 
@@ -652,7 +652,7 @@ function renderCardsFromStorage(e) {
           <span class="exercise-badge">WORKOUT</span>
           <button class="exercise-trash-button">
             <svg class="exercise-trash-icon" width="16" height="16">
-              <use href="/energy_flow/assets/icons-de67b048.svg#icon-trash"></use>
+              <use href="/energy_flow/assets/icons.svg#icon-trash"></use>
             </svg>
           </button>
           <a
@@ -662,14 +662,14 @@ function renderCardsFromStorage(e) {
             <button class="exercise-part-button">
               Start
               <svg class="exercise-btn-icon" width="14" height="14">
-                <use href="/energy_flow/assets/icons-de67b048.svg#icon-arrow-right"></use>
+                <use href="/energy_flow/assets/icons.svg#icon-arrow-right"></use>
               </svg>
             </button>
           </a>
         </div>
         <span class="exercise-part-name">
           <svg class="exercise-part-icon" width="24" height="24">
-            <use href="/energy_flow/assets/icons-de67b048.svg#icon-running-man"></use>
+            <use href="/energy_flow/assets/icons.svg#icon-running-man"></use>
           </svg>
           <p class="exercise-name-text">${name.substring(0, 24) + '...'}</p> 
         </span>
@@ -717,7 +717,7 @@ favoritesContainer.addEventListener('click', function (event) {
           <span class="exercise-badge">WORKOUT</span>
           <button class="exercise-trash-button">
             <svg class="exercise-trash-icon" width="16" height="16">
-              <use href="/energy_flow/assets/icons-de67b048.svg#icon-trash"></use>
+              <use href="/energy_flow/assets/icons.svg#icon-trash"></use>
             </svg>
           </button>
           <a
@@ -727,14 +727,14 @@ favoritesContainer.addEventListener('click', function (event) {
             <button class="exercise-part-button">
               Start
               <svg class="exercise-btn-icon" width="14" height="14">
-                <use href="/energy_flow/assets/icons-de67b048.svg#icon-arrow-right"></use>
+                <use href="/energy_flow/assets/icons.svg#icon-arrow-right"></use>
               </svg>
             </button>
           </a>
         </div>
         <span class="exercise-part-name">
           <svg class="exercise-part-icon" width="24" height="24">
-            <use href="/energy_flow/assets/icons-de67b048.svg#icon-running-man"></use>
+            <use href="/energy_flow/assets/icons.svg#icon-running-man"></use>
           </svg>
           <p class="exercise-name-text">${name.substring(0, 24) + '...'}</p> 
         </span>
