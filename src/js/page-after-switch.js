b5c1adf754c0c382532d9a23d209605e56a62676
IrynaYevcheva <irenmikos@gmail.com>
2024-01-31 01:25:36 +0200

    new

--- src/js/page-after-switch.js
+++ src/js/page-after-switch.js
@@ -8,6 +8,7 @@ const favoritesPage = document.querySelector('#Favorites');
 const homePageMenu = document.querySelector('#Home-menu');
 const favoritesPageMenu = document.querySelector('#Favorites-menu');
 const scrollButton = document.querySelector('.btn-scroll-top');
+const btnMuscles = document.querySelector('#Muscles');
 
 document.addEventListener('DOMContentLoaded', pageSwitch());
 
@@ -21,6 +22,7 @@ function pageSwitch() {
     homePage.classList.add('heder-nav-link-active');
     homePageMenu.classList.add('active-menu-nav-list-item');
     scrollButton.href = './index.html#header';
+    btnMuscles.classList.add('exercise-button-active');
   } else {
     favoritesPage.classList.add('heder-nav-link-active');
     favoritesPageMenu.classList.add('active-menu-nav-list-item');
