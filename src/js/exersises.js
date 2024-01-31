f9d4cc78e321ab1c6e49a365d460820c55eab65b
TetiZ <tommytaba99@gmail.com>
2024-01-31 12:26:56 +0100

    update dumbbell

--- src/js/exersises.js
+++ src/js/exersises.js
@@ -799,12 +799,12 @@ function emptyContent() {
         class="dumbbell-favorites-img"
         srcset="
 
-          /energy_flow/blob/main/src/img/favorites/dumbbell-tab-desc.png?raw=true   116w,
-          /energy_flow/blob/main/src/img/favorites/dumbbell-tab-desc@2x.png?raw=true    231w,
-          /energy_flow/blob/main/src/img/favorites/dumbbell-mob.png?raw=true   85w,
-          /energy_flow/blob/main/src/img/favorites/dumbbell-mob@2x.png?raw=true  170w
+          ./img/favorites/dumbbell-tab-desc.png   116w,
+          ./img/favorites/dumbbell-tab-desc@2x.png    231w,
+          ./img/favorites/dumbbell-mob.png   85w,
+          ./img/favorites/dumbbell-mob@2x.png  170w
         "
-        src="/energy_flow/blob/main/src/img/favorites/dumbbell-mob.png?raw=true"
+        src="./img/favorites/dumbbell-mob.png"
 
         sizes="(min-width: 768px) 116px, (max-width: 767px) 85px"
         alt="dumbbell icon"
