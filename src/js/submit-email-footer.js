4305cafd6b7970a3d8c6b8987084fe2c85282d86
TetiZ <tommytaba99@gmail.com>
2024-02-01 17:51:41 +0100

    footer update

--- src/js/submit-email-footer.js
+++ src/js/submit-email-footer.js
@@ -1,23 +1,11 @@
-import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
-
 const form = document.querySelector('.subs-form');
 const localStorageKey = 'user-email';
-
 const savedEmail = localStorage.getItem(localStorageKey);
-
-if (savedEmail) {
-  form.elements.email.value = savedEmail;
-} else {
-  form.elements.email.value = '';
-}
-
+form.elements.email.value = savedEmail ? savedEmail : '';
 form.addEventListener('input', saveToLocalStorage);
-
 function saveToLocalStorage(evt) {
   localStorage.setItem(localStorageKey, evt.target.value);
 }
-
 async function postEmail(userEmail) {
   try {
     const response = await fetch(
@@ -27,7 +15,7 @@ async function postEmail(userEmail) {
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ email: userEmail }),
+        body: JSON.stringify({ email: userEmail }), // Оновлено поле з emails nа email
       }
     );
     if (!response.ok) {
@@ -36,18 +24,10 @@ async function postEmail(userEmail) {
     }
     const data = await response.json();
     console.log('Дані успішно відправлені', data);
-
-    iziToast.success({
-      title: 'We are excited to have you on board!',
-      message:
-        'Thank you for subscribing to new exercises on Energy Flow. You have just taken a significant step towards improving your fitness and well-being.',
-      position: 'center',
-    });
   } catch (error) {
     console.error('Помилка при відправленні даних', error);
   }
 }
-
 form.addEventListener('submit', async e => {
   e.preventDefault();
   const userEmail = localStorage.getItem(localStorageKey);
@@ -56,6 +36,13 @@ form.addEventListener('submit', async e => {
       await postEmail(userEmail);
       localStorage.removeItem(localStorageKey);
       form.reset();
+
+      iziToast.success({
+        title: 'We are excited to have you on board!',
+        message:
+          'Thank you for subscribing to new exercises on Energy Flow. You have just taken a significant step towards improving your fitness and well-being.',
+        position: 'center',
+      });
     } catch (error) {
       console.log('Помилка при відправленні електронної пошти.');
     }
@@ -63,16 +50,4 @@ form.addEventListener('submit', async e => {
     console.log('Електронна пошта не введена.');
   }
 });
-
-// MODAL
-const closeBtnFooter = document.querySelector('.footer-close-btn');
-const backDropFooter = document.querySelector('.footer-modal-backdrop');
-
-const openFooterModalBtn = document.querySelector('.footer-team-link');
-openFooterModalBtn.addEventListener('click', function () {
-  backDropFooter.classList.add('is-open');
-});
-
-closeBtnFooter.addEventListener('click', function () {
-  backDropFooter.classList.remove('is-open');
-});
+const form = document.querySelector('.subs-form');
