b31e1a85846421ab945b3dcaf726f3a8b6c6cc81
TetiZ <tommytaba99@gmail.com>
2024-02-02 10:03:22 +0100

    delete part with team - bugfix

--- src/js/submit-email-footer.js
+++ src/js/submit-email-footer.js
@@ -1,7 +1,9 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
 const form = document.querySelector('.subs-form');
 const localStorageKey = 'user-email';
 const savedEmail = localStorage.getItem(localStorageKey);
-
 form.elements.email.value = savedEmail ? savedEmail : '';
 form.addEventListener('input', saveToLocalStorage);
 function saveToLocalStorage(evt) {
@@ -20,19 +22,14 @@ async function postEmail(userEmail) {
       }
     );
     if (!response.ok) {
+      console.error('Код помилки:', response.status);
       throw new Error('Мережевий запит не був успішним');
     }
     const data = await response.json();
-
-    iziToast.success({
-      title: 'We are excited to have you on board!',
-      message:
-        'Thank you for subscribing to new exercises on Energy Flow. You have just taken a significant step towards improving your fitness and well-being.',
-      position: 'center',
-    });
-
     console.log('Дані успішно відправлені', data);
-  } catch (error) {}
+  } catch (error) {
+    console.error('Помилка при відправленні даних', error);
+  }
 }
 form.addEventListener('submit', async e => {
   e.preventDefault();
@@ -55,17 +52,4 @@ form.addEventListener('submit', async e => {
   } else {
     console.log('Електронна пошта не введена.');
   }
-});
-
-// MODAL
-const closeBtnFooter = document.querySelector('.footer-close-btn');
-const backDropFooter = document.querySelector('.footer-modal-backdrop');
-
-const openFooterModalBtn = document.querySelector('.footer-team-link');
-openFooterModalBtn.addEventListener('click', function () {
-  backDropFooter.classList.add('is-open');
-});
-
-closeBtnFooter.addEventListener('click', function () {
-  backDropFooter.classList.remove('is-open');
 });
