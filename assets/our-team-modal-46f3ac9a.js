ce19947b46070e7927d1263fb47bad50bd8d542e
TetiZ <140248456+TetiZ@users.noreply.github.com>
2024-02-02 09:10:48 +0000

    Deploying to gh-pages from @ TetiZ/energy_flow@b1d0f7dec6769a0967403475ab916f93fcb998e2 🚀

---
+++ assets/our-team-modal-46f3ac9a.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".footer-close-btn"),o=document.querySelector(".footer-modal-backdrop"),t=document.querySelector(".footer-team-link");t.addEventListener("click",function(){o.classList.add("is-open")});e.addEventListener("click",function(){o.classList.remove("is-open")});
+//# sourceMappingURL=our-team-modal-46f3ac9a.js.map
